54aa47980ac4c555f0e5505892b04a01315fb53e
Katy St Sauveur <katyrogowski@yahoo.com>
2021-06-22 14:31:06 -0700

    Implemented basic code for TicTacToe component

---
+++ src/components/TicTacToe.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class='tictactoe-board'>
+    <div v-for="(n, i) in 3">
+      <div v-for="(n, j) in 3">
+        <cell :value="board[i][j]"></cell>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+  
+    data() {
+      return {
+        board: [
+            ['', '', ''], 
+            ['', 'x', ''],
+            ['', '', '']
+        ]
+              
+      }
+    }
+  }
+</script>
+<style>
+  .tictactoe-board {
+    display: flex;
+    flex-wrap: wrap;
+    
+    width: 204px;
+    height: 204px;
+  }
+</style>
