1fd68df1dca31b52fad7d5f841d932855cc80319
Katy St Sauveur <katyrogowski@yahoo.com>
2021-06-22 15:02:05 -0700

    Import components into main.js file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import TicTacToe from './components/TicTacToe.vue'
+import Cell from './components/Cell.vue'
+
+Vue.component('tic-tac-toe', TicTacToe);
+Vue.component('cell', Cell);
 
 new Vue({
   el: '#app',
