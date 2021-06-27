a0be8961b11d87c70fbcf17d5cddbb8badfcea8b
Katy St Sauveur <katyrogowski@yahoo.com>
2021-06-26 21:42:07 -0700

    Implelment minmax function

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -25,6 +25,34 @@
       
       this.$forceUpdate();
       }
+    }, 
+    minimax(board, player, depth = 1) {
+      if (board.isGameOver()) {
+        return {
+          score: board.getScore() + depth,
+          move: null
+        }
+      }
+      let bestScore = player === 'o' ? -10000 : 10000;
+      let bestMove = null;
+      
+      let moves = board.getPossibleMoves();
+      for (let i = 0; i < moves.length; i++) {
+        let move = moves[i];
+        let newBoard = board.clone();
+        newBoard.doMove(move.x, move.y, player)
+        
+        const score = this.minimax(newBoard, player === 'x' ? 'o': 'x', depth + 1).score;
+        
+        if ((player === 'o' && score > bestScore) || (player === 'x' && score < bestScore)) {
+          bestScore = score;
+          bestMove = move;
+        }
+      }
+      return {
+        score: bestScore,
+        move: bestMove,
+      }
     }
   }
 </script>
