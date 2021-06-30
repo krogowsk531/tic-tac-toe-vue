3aef02df3a445a386a100482c5669f3e79291a3e
Katy St Sauveur <katyrogowski@yahoo.com>
2021-06-30 16:03:35 -0700

    Implement code for scoring

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -2,13 +2,18 @@
   <div class='tictactoe-board'>
     <div v-for="(n, i) in 3">
       <div v-for="(n, j) in 3">
-        <cell @click='performMove(i, j)' :value="board.cells[i][j]"></cell>
+        <cell @click='performMove(j, i)' :value="board.cells[j][i]"></cell>
       </div>
+    </div>
+    
+    <div class='game-over-text' v-if='gameOver'>
+      {{ gameOverText }}
     </div>
   </div>
 </template>
 <script>
     import Board from './Board'
+    
   export default {
   
     data() {
@@ -19,13 +24,33 @@
     
     methods: {
       performMove(x, y) {
-      if (!this.board.doMove(x, y, 'x')) {
+      if (this.gameOver) {
         return;
       }
       
+      if (! this.board.doMove(x, y, 'x')) {
+        return 
+      };
+      
       this.$forceUpdate();
+      
+      if (this.board.isGameOver()) {
+        this.gameOver = true;
+        this.gameOverText = this.board.playerHas3InARow('x') ? 'You win!' : 'Draw'
+        return 
       }
+      
+      let aiMove = this.minimax(this.board.clone(), 'o')
+      this.board.doMove(aiMove.move.x, aiMove.move.y, 'o')
+      
+      if (this.board.isGameOver()) {
+        this.gameOver = true;
+        this.gameOverText = this.board.playerHas3InARow('o') ? 'You lose! : 'Draw';
+      }
+      
+      this.$forceUpdate()
     }, 
+    
     minimax(board, player, depth = 1) {
       if (board.isGameOver()) {
         return {
@@ -33,6 +58,7 @@
           move: null
         }
       }
+      
       let bestScore = player === 'o' ? -10000 : 10000;
       let bestMove = null;
       
@@ -55,6 +81,7 @@
       }
     }
   }
+}
 </script>
 <style>
   .tictactoe-board {
